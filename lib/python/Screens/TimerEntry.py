7700b9fc58e8f45ad265ee8d46a3dbb4ac482a0d
hdeeco <git.hub@mail.com>
2014-03-14 13:36:56 +0100

    changed keymap

--- lib/python/Screens/TimerEntry.py
+++ lib/python/Screens/TimerEntry.py
@@ -119,9 +119,12 @@ class TimerEntry(Screen, ConfigListScreen):
 			weekday = int(strftime("%u", localtime(self.timer.begin))) - 1
 			day[weekday] = 1
 
+#		self.timerentry_justplay = ConfigSelection(choices = [
+#			("zap", _("zap")), ("record", _("record")), ("zap+record", _("zap and record"))],
+#			default = {0: "record", 1: "zap", 2: "zap+record"}[justplay + 2*always_zap])
 		self.timerentry_justplay = ConfigSelection(choices = [
-			("zap", _("zap")), ("record", _("record")), ("zap+record", _("zap and record"))],
-			default = {0: "record", 1: "zap", 2: "zap+record"}[justplay + 2*always_zap])
+			("zap", _("zap"))],
+			default = {0: "zap", 1: "zap", 2: "zap"}[justplay + 2*always_zap])
 		if SystemInfo["DeepstandbySupport"]:
 			shutdownString = _("go to deep standby")
 		else:
