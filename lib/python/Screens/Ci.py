8e345cb876f3d283441cc85c0cce4d5b9cc096a9
hdeeco <git.hub@mail.com>
2016-06-15 19:32:21 +0200

    fix

--- lib/python/Screens/Ci.py
+++ lib/python/Screens/Ci.py
@@ -45,7 +45,7 @@ def InitCiConfig():
 		config.ci[slot].static_pin = ConfigPIN(default = 0)
 		config.ci[slot].show_ci_messages = ConfigYesNo(default = True)
 		if SystemInfo["CommonInterfaceSupportsHighBitrates"]:
-			config.ci[slot].canHandleHighBitrates = ConfigSelection(choices = [("no", _("No")), ("yes", _("Yes"))], default = "no")
+			config.ci[slot].canHandleHighBitrates = ConfigSelection(choices = [("no", _("No")), ("yes", _("Yes"))], default = "yes")
 			config.ci[slot].canHandleHighBitrates.slotid = slot
 			config.ci[slot].canHandleHighBitrates.addNotifier(setCIBitrate)
 		if SystemInfo["CommonInterfaceCIDelay"]:
